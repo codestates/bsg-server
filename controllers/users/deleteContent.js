ddbb035ebcb8ff94fdc6acc1ae7bc5557cc45b6a
Yongho5580 <Yongho5580@users.noreply.github.com>
2021-02-08 14:59:08 +0900

    Completed at 2020-02-08

--- controllers/users/deleteContent.js
+++ controllers/users/deleteContent.js
@@ -1,7 +1,7 @@
-const { user, usercontent } = require('../../models');
+const { user, userContent } = require('../../models');
 
 // COMPLETED
-
+//
 module.exports =  async (req, res) => { //req내부에는 user.id가 있다.
   const deleteUser = await user.findOne({
     where: {
@@ -11,7 +11,7 @@ module.exports =  async (req, res) => { //req내부에는 user.id가 있다.
 
   // 누른 사람의 아이디가
 
-  const userContentData = await usercontent.findOne({
+  const userContentData = await userContent.findOne({
     where: {
       userid: deleteUser.id
     }
@@ -20,7 +20,7 @@ module.exports =  async (req, res) => { //req내부에는 user.id가 있다.
   if (deleteUser.id !== userContentData.userid) {
     res.status(400).json({ message: "no" })
   } else {
-    usercontent.destroy({
+    userContent.destroy({
       where:({
         id: req.body.contentId
       })
